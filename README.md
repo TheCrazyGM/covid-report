6eeacd0abab82f8b9f46a96619c4cd697d8d53ca
Michael Garcia <thecrazygm@gmail.com>
2020-04-01 16:39:10 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # covid-report
 Automated report of European Union CDC statistics
+
+Assumes you have "STEEM_WIF" defined in env
---
+++ app.py
@@ -0,0 +1,41 @@
+from datetime import date, datetime, timedelta
+import os
+from io import StringIO
+from pprint import pprint
+
+import requests
+from beem import Steem
+from beem.wallet import Wallet
+from pandas import read_csv
+from sqlalchemy import create_engine
+wif = os.environ['STEEM_WIF']
+
+hv = Steem(node="https://anyx.io", keys=wif, nobroadcast=True)
+w = Wallet(steem_instance=hv)
+author = w.getAccountFromPrivateKey(wif)
+engine = create_engine('sqlite:///covid.db')
+covid_cvs = requests.get(
+    'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv')
+yesterday = datetime.now() + timedelta(days=-1)
+covid_day = yesterday.strftime("%d/%m/%Y")
+df = read_csv(StringIO(covid_cvs.text))
+sql = df.to_sql("Covid", con=engine, if_exists="replace")
+result = engine.execute(f'SELECT * FROM Covid where dateRep="{covid_day}"')
+data = result.fetchall()
+title = f"European Centre for Disease Prevention and Control Report for Date {covid_day}"
+table = "|: Date Reported :|: Cases :|: Deaths :|: Country / Territory :| \n "
+table += "| ------------- |------:| ------:| :------------------ |  \n "
+for row in data:
+    table += f'|{row["dateRep"]} | {row["cases"]}| {row["deaths"]}| {row["countriesAndTerritories"]} | \n'
+
+body = f"""
+# ECDC Automated Report
+This is a work in progress, the data is gathered daily from the European Union CDC (which I find to be more believable than our government at this moment) If you would like to contribute, please feel free to check out the [GitHub Repo here](https://github.com/TheCrazyGM/covid-report).
+## Report for end of day Yesterday - {yesterday}
+![](https://www.ecdc.europa.eu/profiles/custom/ecdc/themes/anthrax/images/logo-ecdc.png)
+{table}
+"""
+tags = ['covid-19', 'coronavirus', 'covid', 'quarantine']
+
+tx = hv.post(title=title, body=body, author=author, permlink=f"Report-{covid_day}")
+pprint(tx)
