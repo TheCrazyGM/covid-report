1566ea1cb4a9bee565630bc7f466dd9dcce15546
Michael Garcia <thecrazygm@gmail.com>
2020-08-23 13:17:18 -0400

    md formatting

--- covid/__main__.py
+++ covid/__main__.py
@@ -34,14 +34,11 @@ def main():
     table += ''.join(f'|{row["dateRep"]} | {row["cases"]}| {row["deaths"]}| {row["countriesAndTerritories"]} | \n' for row in data)
 
     body = f"""
-    # ECDC Automated Report
-
+    #ECDC Automated Report
     This is a work in progress, the data is gathered daily from the European Union CDC 
     If you would like to contribute, please feel free to check out the [GitHub Repo here](https://github.com/TheCrazyGM/covid-report).
-
-    ## Report for end of day Yesterday - {covid_day}
+    ##Report for end of day Yesterday - {covid_day}
     ![](https://www.ecdc.europa.eu/profiles/custom/ecdc/themes/anthrax/images/logo-ecdc.png)
-
     {table}
     """
     tags = ['coronavirus', 'covid', 'covid-19', 'quarantine']
